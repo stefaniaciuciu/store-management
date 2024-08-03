026f039a1f87dd6f12c6bfd6c0103f210c22f937
stefaniaciuciu <mariastefaniaciuciu@gmail.com>
2024-08-03 23:55:12 +0300

    Created all tests for User controller and service

--- src/test/java/com/store/project/controller/TestUserController.java
+++ src/test/java/com/store/project/controller/TestUserController.java
@@ -2,6 +2,7 @@ package com.store.project.controller;
 
 import com.store.project.exceptions.CustomExceptions;
 import com.store.project.model.User;
+import com.store.project.modelDTO.UserUpdateDTO;
 import com.store.project.service.UserService;
 import com.store.project.util.Util;
 import org.junit.jupiter.api.Test;
@@ -44,6 +45,21 @@ public class TestUserController {
     }
 
     @Test
+    public void testRegisterUserAlreadyExists() throws Exception {
+        User user = Util.createUserForTests();
+
+        doThrow(new CustomExceptions.UserAlreadyExistsException(String.format("User already exists: %s", user.getEmail())))
+                .when(userService).registerUser(any(User.class));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/user/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(user)))
+                .andExpect(MockMvcResultMatchers.status().isConflict());
+
+        verify(userService, times(1)).registerUser(any(User.class));
+    }
+
+    @Test
     public void testRegisterUserInvalidEmail() throws Exception {
         User user = Util.createUserForTests();
 
@@ -56,6 +72,139 @@ public class TestUserController {
                 .andExpect(MockMvcResultMatchers.status().isBadRequest());
 
         verify(userService, times(1)).registerUser(any(User.class));
+    }
+
+    @Test
+    public void testRegisterUserInvalidPassword() throws Exception {
+        User user = Util.createUserForTests();
+
+        doThrow(new CustomExceptions.InvalidPasswordException("Invalid password format"))
+                .when(userService).registerUser(any(User.class));
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/user/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(user)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+        verify(userService, times(1)).registerUser(any(User.class));
+    }
+
+    @Test
+    public void testUpdatePasswordOK() throws Exception {
+        User user = Util.createUserForTests();
+        String newPassword = "newPassword";
+        String userUpdateDTOJson = String.format(
+                "{\"email\":\"%s\",\"oldPassword\":\"%s\",\"newPassword\":\"%s\",\"confirmPassword\":\"%s\"}",
+                user.getEmail(), user.getPassword(), newPassword, newPassword
+        );
+
+        when(userService.updatePassword(user.getEmail(),user.getPassword(), newPassword, newPassword))
+                .thenReturn(user);
+        mockMvc.perform(MockMvcRequestBuilders.patch("/user/updatePassword")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userUpdateDTOJson))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        verify(userService, times(1)).updatePassword(user.getEmail(),user.getPassword(),
+                newPassword, newPassword);
+    }
+
+    @Test
+    public void testUpdatePasswordFailUserNotFound() throws Exception {
+        User user = Util.createUserForTests();
+        String newPassword = "newPassword";
+        String userUpdateDTOJson = String.format(
+                "{\"email\":\"%s\",\"oldPassword\":\"%s\",\"newPassword\":\"%s\",\"confirmPassword\":\"%s\"}",
+                user.getEmail(), user.getPassword(), newPassword, newPassword
+        );
+
+        doThrow(new CustomExceptions.UserNotFoundException("User not found"))
+                .when(userService).updatePassword(user.getEmail(),user.getPassword(), newPassword, newPassword);
+
+        mockMvc.perform(MockMvcRequestBuilders.patch("/user/updatePassword")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userUpdateDTOJson))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+
+        verify(userService, times(1)).updatePassword(user.getEmail(),user.getPassword(),
+                newPassword, newPassword);
+    }
+
+    @Test
+    public void testUpdatePasswordFailIvalidPassword() throws Exception {
+        User user = Util.createUserForTests();
+        String newPassword = "newPassword";
+        String userUpdateDTOJson = String.format(
+                "{\"email\":\"%s\",\"oldPassword\":\"%s\",\"newPassword\":\"%s\",\"confirmPassword\":\"%s\"}",
+                user.getEmail(), user.getPassword(), newPassword, newPassword
+        );
+
+        doThrow(new CustomExceptions.InvalidPasswordException("User not found"))
+                .when(userService).updatePassword(user.getEmail(),user.getPassword(), newPassword, newPassword);
+
+        mockMvc.perform(MockMvcRequestBuilders.patch("/user/updatePassword")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userUpdateDTOJson))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+        verify(userService, times(1)).updatePassword(user.getEmail(),user.getPassword(),
+                newPassword, newPassword);
+    }
+
+    @Test
+    public void testLoginOk() throws Exception {
+        User user = Util.createUserForTests();
+        String userLoginDTOJson = String.format(
+                "{\"email\":\"%s\",\"password\":\"%s\"}",
+                user.getEmail(), user.getPassword()
+        );
+
+        when(userService.loginUser(user.getEmail(),user.getPassword()))
+                .thenReturn(user);
+        mockMvc.perform(MockMvcRequestBuilders.get("/user/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userLoginDTOJson))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        verify(userService, times(1)).loginUser(user.getEmail(),user.getPassword());
+    }
+
+    @Test
+    public void testLoginFailUserNotFound() throws Exception {
+        User user = Util.createUserForTests();
+        String userLoginDTOJson = String.format(
+                "{\"email\":\"%s\",\"password\":\"%s\"}",
+                user.getEmail(), user.getPassword()
+        );
+
+        doThrow(new CustomExceptions.UserNotFoundException("User not found"))
+                .when(userService).loginUser(user.getEmail(),user.getPassword());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/user/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userLoginDTOJson))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+
+        verify(userService, times(1)).loginUser(user.getEmail(),user.getPassword());
+    }
+
+    @Test
+    public void testLoginFailInvalidPassword() throws Exception {
+        User user = Util.createUserForTests();
+        String userLoginDTOJson = String.format(
+                "{\"email\":\"%s\",\"password\":\"%s\"}",
+                user.getEmail(), user.getPassword()
+        );
+
+        doThrow(new CustomExceptions.InvalidPasswordException("Password is invalid!"))
+                .when(userService).loginUser(user.getEmail(),user.getPassword());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/user/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userLoginDTOJson))
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
+
+        verify(userService, times(1)).loginUser(user.getEmail(),user.getPassword());
     }
 
 }
--- src/test/java/com/store/project/service/TestUserService.java
+++ src/test/java/com/store/project/service/TestUserService.java
@@ -11,7 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-public class UserServiceTest {
+public class TestUserService {
 
     @Autowired
     private UserService userService;
