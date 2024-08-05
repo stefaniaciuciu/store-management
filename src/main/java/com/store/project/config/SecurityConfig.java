236a53bed961eb49967a832ab8f9a8b67ab93948
stefaniaciuciu <mariastefaniaciuciu@gmail.com>
2024-08-05 09:10:21 +0300

    Created password encryption

---
+++ src/main/java/com/store/project/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.store.project.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers("/user/**")
+                                .permitAll()
+                )
+        ;
+
+        return http.build();
+    }
+
+}
